ffdc3549823d25c4acb01aaf7570401d54c21a00
juancarlos-star <jcpublicidad4@gmail.com>
2026-09-13 01:01:03 +0200

    Add files via upload

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "cuenta-clara-v1";
+const CACHE_NAME = "cuenta-clara-v2";
 const APP_SHELL = ["/", "/index.html", "/manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
